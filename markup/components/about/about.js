487dbaf9f39cbbc225aaa39f3a1e6279970052c9
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-12-09 15:54:00 +0300

    Добавлена анимация для блока about

--- markup/components/about/about.js
+++ markup/components/about/about.js
@@ -1,1 +1,17 @@
+/*eslint-disable*/
+new WOW().init();
 
+var $aboutBlockOffset = $('.about').offset().top;
+var $setActiveToPencilBlock = false;
+var $pencilBlock = $('.pencil-block');
+
+$(window).on('scroll', function () {
+    var $scrollWindow = $(this).scrollTop();
+    if(!$setActiveToPencilBlock){
+        if ($scrollWindow >= ($aboutBlockOffset - 200)) {
+            $pencilBlock.addClass('active');
+            $setActiveToPencilBlock = true;
+        }
+    }
+});
+/*eslint-enable*/
