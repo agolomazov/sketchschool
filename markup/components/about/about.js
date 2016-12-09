66c093874678bb40bac5972bd29869ed7cb7f16e
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-12-09 17:05:37 +0300

    Добавлен адаптив для блока about

--- markup/components/about/about.js
+++ markup/components/about/about.js
@@ -1,17 +1,40 @@
 /*eslint-disable*/
-new WOW().init();
 
-var $aboutBlockOffset = $('.about').offset().top;
-var $setActiveToPencilBlock = false;
-var $pencilBlock = $('.pencil-block');
 
-$(window).on('scroll', function () {
-    var $scrollWindow = $(this).scrollTop();
-    if(!$setActiveToPencilBlock){
-        if ($scrollWindow >= ($aboutBlockOffset - 200)) {
-            $pencilBlock.addClass('active');
-            $setActiveToPencilBlock = true;
-        }
+$(document).ready(function(){
+    var wow = new WOW();
+
+    var $aboutBlock = $('.about');
+    var $aboutBlockOffset = $aboutBlock ? $aboutBlock.offset().top : 0;
+    var $setActiveToPencilBlock = false;
+    var $pencilBlock = $('.pencil-block');
+    var $wowInit = false;
+    var $windowWidth = $(window).width();
+
+    if($windowWidth > 780 && !$wowInit){
+        wow.init();
     }
+
+
+    $(window).on('resize', function(){
+        var $windowWidth = $(this).width();
+        if($windowWidth > 780 && !$wowInit){
+            wow.init();
+        } else if($windowWidth <= 780 && $wowInit){
+            wow.disabled();
+        }
+    });
+
+    $(window).on('scroll', function () {
+        var $scrollWindow = $(this).scrollTop();
+        if(!$setActiveToPencilBlock){
+            if ($scrollWindow >= ($aboutBlockOffset - 200)) {
+                $pencilBlock.addClass('active');
+                $setActiveToPencilBlock = true;
+            }
+        }
+    });
 });
+
+
 /*eslint-enable*/
